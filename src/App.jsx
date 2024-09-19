cdd29b815fa03c70deb8067e2240391395300da0
TabulaWeb <tabulaweb@mail.ru>
2024-09-19 21:19:42 +0300

    remove loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import React, { lazy, Suspense } from 'react';
-import { Loader } from './components/landing/loader';
+import { lazy } from 'react';
 
-const Marquee = lazy(() => import('./components/marquee'))
-const Header = lazy(() => import('./components/header'))
-const Home = lazy(() => import('./components/landing/home'))
-const Devider = lazy(() => import('./components/devider'))
-const Idea = lazy(() => import('./components/landing/idea'))
-const IdeaDescription = lazy(() => import('./components/landing/idea-description'))
-const Program = lazy(() => import('./components/landing/program'))
-const Info = lazy(() => import('./components/landing/info'))
-const InfoContact = lazy(() => import('./components/landing/info-contact'))
-const Location = lazy(() => import('./components/landing/location'))
-const Organiser = lazy(() => import('./components/landing/organiser'))
-const Comics = lazy(() => import('./components/landing/comics'))
-const Community = lazy(() => import('./components/landing/community'))
-const Partners = lazy(() => import('./components/landing/partners'))
-const Questions = lazy(() => import('./components/landing/questions'))
-const Footer = lazy(() => import('./components/landing/footer'))
-
+ import Marquee from './components/marquee'
+import Header from './components/header'
+import Devider from './components/devider'
+import Home from './components/landing/home'
+import Idea from './components/landing/idea'
+import IdeaDescription from './components/landing/idea-description'
+import Program from './components/landing/program/index'
+import Info from './components/landing/info'
+import InfoContact from './components/landing/info-contact'
+import Location from './components/landing/location'
+import Organiser from './components/landing/organiser'
+import Comics from './components/landing/comics'
+import Community from './components/landing/community'
+import Partners from './components/landing/partners'
+import Questions from './components/landing/questions'
+import Footer from './components/landing/footer'
 
 function App() {
 
   return (
-    <Suspense fallback={<Loader/>}>
+    <>
       <Marquee />
       <Header />
       <main>
-        <Home />
+        <Home/>
         <Devider />
         <Idea />
         <IdeaDescription />
@@ -41,7 +39,7 @@ function App() {
         <Questions />
       </main>
       <Footer />
-    </Suspense>
+    </>
   )
 }
 
