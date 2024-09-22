7ce6fb90087daf7b9857a376521aca64ee23508f
TabulaWeb <tabulaweb@mail.ru>
2024-09-22 12:52:09 +0300

    remove loading

--- src/app/page.js
+++ src/app/page.js
@@ -3,54 +3,22 @@ import { Suspense } from 'react';
 import dynamic from 'next/dynamic'
 import Loading from './loading';
 
-const Marquee = dynamic(() => import('./components/marquee'), {
-  loading: () => <Loading/>,
-});
-const Header = dynamic(() => import('./components/header'), {
-  loading: () => <Loading/>,
-});
-const Devider = dynamic(() => import('./components/devider'), {
-  loading: () => <Loading/>,
-});
-const FirsSection = dynamic(() => import('./components/landing/home'), {
-  loading: () => <Loading/>,
-});
-const Idea = dynamic(() => import('./components/landing/idea'), {
-  loading: () => <Loading/>,
-});
-const IdeaDescription = dynamic(() => import('./components/landing/idea-description'), {
-  loading: () => <Loading/>,
-});
-const Program = dynamic(() => import('./components/landing/program'), {
-  loading: () => <Loading/>,
-});
-const Info = dynamic(() => import('./components/landing/info'), {
-  loading: () => <Loading/>,
-});
-const InfoContact = dynamic(() => import('./components/landing/info-contact'), {
-  loading: () => <Loading/>,
-});
-const Location = dynamic(() => import('./components/landing/location'), {
-  loading: () => <Loading/>,
-});
-const Organiser = dynamic(() => import('./components/landing/organiser'), {
-  loading: () => <Loading/>,
-});
-const Comics = dynamic(() => import('./components/landing/comics'), {
-  loading: () => <Loading/>,
-});
-const Community = dynamic(() => import('./components/landing/community'), {
-  loading: () => <Loading/>,
-});
-const Partners = dynamic(() => import('./components/landing/partners'), {
-  loading: () => <Loading/>,
-});
-const Questions = dynamic(() => import('./components/landing/questions'), {
-  loading: () => <Loading/>,
-});
-const Footer = dynamic(() => import('./components/landing/footer'), {
-  loading: () => <Loading/>,
-});
+const Marquee = dynamic(() => import('./components/marquee'));
+const Header = dynamic(() => import('./components/header'));
+const Devider = dynamic(() => import('./components/devider'));
+const FirsSection = dynamic(() => import('./components/landing/home'));
+const Idea = dynamic(() => import('./components/landing/idea'));
+const IdeaDescription = dynamic(() => import('./components/landing/idea-description'));
+const Program = dynamic(() => import('./components/landing/program'));
+const Info = dynamic(() => import('./components/landing/info'));
+const InfoContact = dynamic(() => import('./components/landing/info-contact'));
+const Location = dynamic(() => import('./components/landing/location'));
+const Organiser = dynamic(() => import('./components/landing/organiser'));
+const Comics = dynamic(() => import('./components/landing/comics'));
+const Community = dynamic(() => import('./components/landing/community'));
+const Partners = dynamic(() => import('./components/landing/partners'));
+const Questions = dynamic(() => import('./components/landing/questions'));
+const Footer = dynamic(() => import('./components/landing/footer'));
 
 export default function Home() {
   return (
