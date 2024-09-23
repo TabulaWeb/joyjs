36d96a5f76ede31f857f13193cb80f06b0cd7ff7
TabulaWeb <tabulaweb@mail.ru>
2024-09-24 00:39:07 +0300

    add preload font

--- app/layout.js
+++ app/layout.js
@@ -4,32 +4,45 @@ import "./globals.css";
 const unboundedExtraLight = localFont({
   src: "../fonts/Unbounded-ExtraLight.woff",
   variable: "--font-unbounded-extraLight",
+  preload: true
 });
 
 const unboundedLight = localFont({
   src: "../fonts/Unbounded-Light.woff",
   variable: "--font-unbounded-light",
+  preload: true
 });
 
 const unboundedMedium = localFont({
   src: "../fonts/Unbounded-Medium.woff",
   variable: "--font-unbounded-medium",
+  preload: true
 })
 
 const yapariTrialMedium = localFont({
   src: "../fonts/YapariTrial-Medium.woff",
   variable: "--font-yaparitrial-medium",
+  preload: true
 })
 
 const yapariTrialSemiBold = localFont({
   src: "../fonts/YapariTrial-SemiBold.woff",
   variable: "--font-yaparitrial-semibold",
+  preload: true
 })
 
 export const metadata = {
   title: "JoyJS - митапы для разработчиков в Пскове",
   description: "JoyJS это митап на котором тебя ждёт программа с четырьмя докладами от экспертов. В перерывах между выступлениями будет отличная возможность пообщаться, обменяться идеями и завести новые знакомства. Идея \"Joy.JS\" заключается в развитии IT-культуры в регионах. Для формирования сообщества был выбран Псков, так как это родной город создателя Joy.JS Алексея Табула.",
   icon: './icon.png',
+  siteName: 'joyjs',
+  keywords: [
+    'Митап в пскове',
+    'Митап',
+    'Митап по JavaScript',
+    'Алексей Табула',
+    'Митап по фронтенду',
+  ],
   metadataBase: new URL('https://joyjs.ru'),
   alternates: {
     canonical: '/',
