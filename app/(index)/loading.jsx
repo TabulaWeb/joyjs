bab151d461e34067f38396bd8c872713f5f57c68
TabulaWeb <tabulaweb@mail.ru>
2024-09-23 17:44:48 +0300

    fix loading page

--- app/(index)/loading.jsx
+++ app/(index)/loading.jsx
@@ -1,45 +1,5 @@
-'use client'
-
-import Image from 'next/image'
-import { styled } from '@linaria/react'
-import ImageSmile from '../../image/smile.png'
-
 export default function Loading() {
 	return (
-		<Main>
-			<Smile 
-				src={ImageSmile}
-				priority={true}
-				alt='smile'
-				role='img'
-				width={200}
-				height={200}
-			/>
-		</Main>
+		<p>Страница скоро загрузится, подождите пожалуйста</p>
 	)
 }
-
-const Main = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	height: 100dvh;
-	position: absolute;
-	z-index: 10;
-	top: 0;
-	left: 0;
-`
-
-const Smile = styled(Image)`
-	@keyframes blob {
-		0% {
-			transform: scale(0);
-		}
-		100% {
-			transform: scale(1);
-		}
-	}
-
-	animation: blob 1s ease;
-`
