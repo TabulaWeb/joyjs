4cb61f887ca44835bda0519f3a7ced5e5a92af8a
TabulaWeb <tabulaweb@mail.ru>
2024-09-24 11:06:34 +0300

    output dir

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withLinaria = require('next-with-linaria')
 
 /** @type {import('next-with-linaria').LinariaConfig} */
 const config = {
+  distDir: 'build',
   logging: {
     fetches: {
       fullUrl: true
